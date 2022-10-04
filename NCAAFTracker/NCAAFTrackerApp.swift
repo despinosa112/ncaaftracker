32486a7176284436b590edf2cf4ab5e229a7e323
Daniel Espinosa <despinosa112@gmail.com>
2022-10-03 23:47:13 -0400

    Initial Commit

---
+++ NCAAFTracker/NCAAFTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCAAFTrackerApp.swift
+//  NCAAFTracker
+//
+//  Created by Daniel Espinosa on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct NCAAFTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
